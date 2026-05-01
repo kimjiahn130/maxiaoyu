ae0d91b084e7e0804f74c516562dda08beb1a259
안안 <kimjiahn130@gmail.com>
2026-05-02 01:14:51 +0900

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import Navbar from "@/components/Navbar";
 export const metadata: Metadata = {
   title: "MA XIAO YU_马小宇",
   description: "Dedicated to actor Ma Xiao-yu",
+  // 카톡이나 SNS 공유용 제목을 아래에 한 번 더 적어주는 거예요
+  openGraph: {
+    title: "MA XIAO YU_马小宇",
+    description: "Dedicated to actor Ma Xiao-yu",
+  },
 };
 
 export default function RootLayout({
